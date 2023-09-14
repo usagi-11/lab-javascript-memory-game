1081ad662f28778ae519be954ed30699f7d44411
Telma Irina <telmairinacoelho@gmail.com>
2023-09-14 07:36:26 +0100

    incomplete

--- src/memory.js
+++ src/memory.js
@@ -1,18 +1,43 @@
 class MemoryGame {
   constructor(cards) {
     this.cards = cards;
-    // add the rest of the class properties here
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
   }
 
   shuffleCards() {
-    // ... write your code here
+    if (!this.cards) {
+      return undefined;
+    }
+      const shuffleCardArr = [];
+      while (this.cards.length >0) {
+        const randomIndex = Math.floor(Math.random()*this.cards.length)
+        const randomCard = this.cards[randomIndex];
+        shuffleCardArr.push(randomCard);
+        this.cards.splice(randomIndex, 1);
+      }
+      this.cards = shuffleCardArr;
   }
 
   checkIfPair(card1, card2) {
-    // ... write your code here
+      this.pairsClicked +=1;
+      if (card1 === card2) {
+        this.pairsGuessed +=1;
+        return true;
+      }
+      else{
+        return false;
+      }
   }
 
   checkIfFinished() {
-    // ... write your code here
+    const totalPairs = this.cards.length /2;
+    if (this.pairsGuessed === totalPairs) {
+      return true;
+    }
+    else{
+      return false;
+    }
   }
 }
